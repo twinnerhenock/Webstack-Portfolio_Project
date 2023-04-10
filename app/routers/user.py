47c1e4bbcadf0847e203aaa114b29c78527c69b2
twinnerhenock <demessiehe@gmail.com>
2023-04-10 16:11:57 +0300

    create n get user's endpoints

---
+++ app/routers/user.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from .. import models, schemas, utils
+from sqlalchemy.orm import Session
+from ..database import get_db
+from typing import List
+
+router = APIRouter(
+    prefix="/users",
+    tags = ['Users']
+)
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    
+    hashed_pwd = utils.hash(user.password)
+    user.password = hashed_pwd
+
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+@router.get('/{id}')
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'User with id: {id} does not exist')
+    
+    return user
